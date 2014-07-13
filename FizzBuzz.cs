b86822bdb9d11e20198730196997586b75b60374
y-adachi <doruji1982@gmail.com>
2014-07-13 22:42:52 +0900

    add 2 files.
    fizzbuzz and prime number judgement

---
+++ FizzBuzz.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace skillup
+{
+    class FizzBuzz
+    {
+        static void Main(string[] args)
+        {
+            // start
+            Console.WriteLine("Start ------------- ");
+
+            // declare
+            int i = 1;
+            string fizzBuzz = "FizzBuzz";
+            string fizz     = "Fizz";
+            string buzz     = "Buzz";
+
+            // FizzBuzz 
+            while (i <= 100)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine(fizzBuzz);
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine(fizz);
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine(buzz);
+                }
+                else
+                {
+                    Console.WriteLine(i.ToString());
+                }
+                
+                i++;
+            }
+
+            Console.WriteLine("End ------------- ");
+        }
+    }
+}
---
+++ SosuJudge.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SosuJudge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //①与えられた数が素数かどうか調べる
+            //②与えられた数までの素数を列挙する
+            //③処理にかかった時間を計測する
+
+            DateTime startTime = DateTime.Now.ToLocalTime();
+            Console.WriteLine("start: " + startTime.ToString());
+                        
+            // 引数をとる
+            Console.WriteLine(args[0].ToString());
+            int limit = int.Parse(args[0].ToString());
+            PracUtil pracutil = new PracUtil();
+
+            for (int i = 1; i <= limit; i++)
+            {
+                if (pracutil.isSosu(i))
+                {
+                    Console.Write(i);
+                    Console.Write(", ");
+                }
+            }
+
+            // finish
+            DateTime finishTime = DateTime.Now.ToLocalTime(); 
+            Console.WriteLine("");
+            Console.WriteLine("finish: " + finishTime.ToString());
+
+            // stop
+            Console.WriteLine("stop");
+        }
+    }
+
+    public class PracUtil
+    {
+        /// <summary>
+        /// 引数が素数かどうか判定する
+        /// </summary>
+        /// <param name="limit"></param>
+        /// <returns>true = 素数, false = 素数ではない</returns>
+        public Boolean isSosu(int limit)
+        {
+            int count = 0;
+            int i = 1;
+            Boolean judge = false;
+
+            // 引数の値まで繰り返し除算を試みる
+            for (i = 1; i <= limit; i++)
+            {
+
+                int answer = limit % i;
+                if (answer == 0)
+                {
+                    count++;
+                }
+
+                if (count == 2)
+                {
+                    break;
+                }
+            }
+
+            // 素数判定
+            if (i == limit || limit == 1)
+            {
+                judge = true;
+            }
+
+            return judge;
+
+        }
+    }
+}
+
